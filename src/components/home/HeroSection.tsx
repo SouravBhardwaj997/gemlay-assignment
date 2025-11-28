96c099791244af1b2248220c953048ba627e2336
Sourav <souravbhardwaj997@gmail.com>
2025-11-28 12:48:06 +0530

    sadfsdaf

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -1,10 +1,14 @@
+"use client";
 import Image from "next/image";
 
+import { useMediaQuery } from "react-responsive";
+
 export const HeroSection = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 800px)" });
   return (
     <div className="w-full">
       <Image
-        src={"/images/hero.png"}
+        src={isMobile ? "/images/hero-sm.png" : "/images/hero.png"}
         width={1000}
         height={1000}
         className="w-full"
